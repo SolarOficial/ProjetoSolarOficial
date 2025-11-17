232bced0c13408786f62a39889c54f52378ff3b4
Gabriel Batistela <157296840+Gabriel-Batistela@users.noreply.github.com>
2025-11-17 19:05:35 -0300

    Feat: Adicionar o popup da Solaria

--- src/js/script.js
+++ src/js/script.js
@@ -66,3 +66,16 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 
+// Solaria
+
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/691a0ba6ccfc7c195b6c429d/1ja6rr188';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
